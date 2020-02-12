e0842722d9b1a20b5317eacd9bfbbcd0624198aa
Daniel <danielmichalik98@wp.pl>
2020-02-12 22:32:11 +0100

    Add login and logout on homepage

--- GroupChatDjango/GroupChatDjango/urls.py
+++ GroupChatDjango/GroupChatDjango/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include("login.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
