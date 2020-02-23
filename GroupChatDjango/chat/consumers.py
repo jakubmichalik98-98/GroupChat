7a029bebbd88c6fc84e15136a2512b071dc8e9cf
Daniel <danielmichalik98@wp.pl>
2020-02-23 13:34:37 +0100

    Upgrade registration page

--- GroupChatDjango/chat/consumers.py
+++ GroupChatDjango/chat/consumers.py
@@ -6,7 +6,7 @@ import json
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
